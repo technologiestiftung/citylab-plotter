affc8324ed041c8d52153c8c53cbd4536e131e62
Fabian Morón Zirfas <icke@fabianmoronzirfas.me>
2019-05-27 15:26:06 +0200

    always lift to z1 & better file handling

--- fiddle/svg-to-gcode/index.js
+++ fiddle/svg-to-gcode/index.js
@@ -3,15 +3,18 @@ const path = require('path');
 const fs = require('fs');
 
 const opts = {
-  depth: 10,
+  depth: 9,
   unit: 'mm',
   map:'xyz',
   top: -10
 };
 
+const inFile = path.resolve(__dirname, './in-svg/A4-rect-1010_200200.svg');
+const outFile = `${inFile.replace('.svg','.gc')}`;
+console.log(outFile);
 const gcode = svgcode()
-  // .loadFile(path.resolve(__dirname, './in-svg/A4-rect-1010_200200.svg'))
-  .loadFile(path.resolve(__dirname, './in-svg/town-a0-841-1189.svg'))
+  .loadFile(inFile)
+  // .loadFile(path.resolve(__dirname, './in-svg/town-a0-841-1189.svg'))
   .setOptions(opts)
   .generateGcode()
   .getGcode();
@@ -20,7 +23,7 @@ gcode.unshift('G0 Z10'); // lift at start
 gcode.push('G0 Z10'); // left the pen at the end
 gcode.push('G0 X0 Y0'); // Go Home again
 console.log(gcode);
-fs.writeFile(path.resolve(__dirname, './out-gcode/town-A0.gc'), gcode.join('\n'), 'utf8', (err,data)=>{
+fs.writeFile(outFile, gcode.join('\n'), 'utf8', (err,data)=>{
   if (err){
     throw err;
   }
