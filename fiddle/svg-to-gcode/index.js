dd35a3574d8745d0846373af3454eb2108b43e4c
Fabian Morón Zirfas <icke@fabianmoronzirfas.me>
2019-05-27 15:35:41 +0200

    filter some Z0

--- fiddle/svg-to-gcode/index.js
+++ fiddle/svg-to-gcode/index.js
@@ -1,16 +1,26 @@
 const svgcode = require('svgcode');
 const path = require('path');
 const fs = require('fs');
+const meow = require('meow');
 
+const cli = meow(`
+  Usage: node index.js relative/path/to/file.svg
+
+  Will output next to the srcfile with .gz extension.
+`, {});
+
+if (cli.input.length === 0) {
+  cli.showHelp();
+}
 const opts = {
   depth: 9,
   unit: 'mm',
-  map:'xyz',
+  map: 'xyz',
   top: -10
 };
 
-const inFile = path.resolve(__dirname, './in-svg/A4-rect-1010_200200.svg');
-const outFile = `${inFile.replace('.svg','.gc')}`;
+const inFile = path.resolve(process.cwd(), cli.input[0]);
+const outFile = `${inFile.replace('.svg', '.gc')}`;
 console.log(outFile);
 const gcode = svgcode()
   .loadFile(inFile)
@@ -22,9 +32,12 @@ const gcode = svgcode()
 gcode.unshift('G0 Z10'); // lift at start
 gcode.push('G0 Z10'); // left the pen at the end
 gcode.push('G0 X0 Y0'); // Go Home again
+gcode.forEach((ele, i, arr) => {
+  arr[i] = ele.replace('Z0','Z1');
+});
 console.log(gcode);
-fs.writeFile(outFile, gcode.join('\n'), 'utf8', (err,data)=>{
-  if (err){
+fs.writeFile(outFile, gcode.join('\n'), 'utf8', (err, data) => {
+  if (err) {
     throw err;
   }
   console.log(data);
