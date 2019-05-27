0c1f9166dadd66fd076768014743c8b584a584cd
Fabian Morón Zirfas <icke@fabianmoronzirfas.me>
2019-05-27 14:55:54 +0200

    test: town

--- fiddle/svg-to-gcode/index.js
+++ fiddle/svg-to-gcode/index.js
@@ -10,14 +10,17 @@ const opts = {
 };
 
 const gcode = svgcode()
-  .loadFile(path.resolve(__dirname, './in-svg/A4-rect-1010_200200.svg'))
+  // .loadFile(path.resolve(__dirname, './in-svg/A4-rect-1010_200200.svg'))
+  .loadFile(path.resolve(__dirname, './in-svg/town-01.svg'))
   .setOptions(opts)
   .generateGcode()
   .getGcode();
 
+gcode.unshift('G0 Z10'); // lift at start
 gcode.push('G0 Z10'); // left the pen at the end
+gcode.push('G0 X0 Y0'); // Go Home again
 console.log(gcode);
-fs.writeFile(path.resolve(__dirname, './out-gcode/A4.gcode'), gcode.join('\n'), 'utf8', (err,data)=>{
+fs.writeFile(path.resolve(__dirname, './out-gcode/town-A0.gc'), gcode.join('\n'), 'utf8', (err,data)=>{
   if (err){
     throw err;
   }
