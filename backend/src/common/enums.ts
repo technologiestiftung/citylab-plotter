93aa6a1a8f5553c3b27d30b04e2de8deb772cb02
Fabian Morón Zirfas <icke@fabianmoronzirfas.me>
2019-06-04 15:07:18 +0200

    feat(zeroall): Adds the zertoall command

--- backend/src/common/enums.ts
+++ backend/src/common/enums.ts
@@ -1,6 +1,10 @@
 export enum ControlCommands {
   home = '$H\n',
   unlock = '$x\n',
+  zerox0 = 'g92y0\n',
+  zeroy0 = 'g92x0\n',
+  zeroz0 = 'g92z0\n',
+  zeroz3 = 'g92z3\n',
 }
 export enum PlotterStates {
   busy = 'busy',
